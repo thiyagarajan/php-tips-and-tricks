de3eb358f9086bc57f1678edf2703dfd19a61bac
thiyagarajan <thiyagarajannv@yahoo.com>
2011-04-19 13:19:26 +0530

    added mysql connect

--- tips.php
+++ tips.php
@@ -6,3 +6,23 @@ foreach($cookies as $cookie){
   $when=time()-3600;  
   setcookie($cookie, "", $when, "/", $domain);  
 }
+
+Mysql database connect and get table field value 
+================================================
+// database settings   
+$db_address = "hostname";   
+$db_username = "username";   
+$db_password = "password";   
+$tb_database = "databasename";   
+$table="tbl_users";   
+// connect  
+$conn = mysql_connect($db_address, $db_username, $db_password) or die ('Error connecting to database '.$db_address);  
+// select the database  
+mysql_select_db($tb_database);  
+  
+// get the data and add to an array  
+$query="SELECT * FROM $table;";  
+$result=mysql_query($query);  
+while($row=mysql_fetch_assoc($result)){  
+  $users[]=$row;    
+}
